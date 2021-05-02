95ca6ffa15c641cdfd04c28c2fedbadea6861dd5
Lorenzo <stillduke@gmail.com>
2021-05-02 13:28:22 +0200

    Inizializzata la tableview

--- CustomTableView/TableView/CustomTableView.swift
+++ CustomTableView/TableView/CustomTableView.swift
@@ -11,6 +11,8 @@ class CustomTableView: UIView, UITableViewDelegate, UITableViewDataSource {
     
     static public let tableViewID = "CustomTableView"
     
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var tableViewCustom: UITableView!
     
     //MARK: - Class Inits
     override init(frame: CGRect) {
@@ -25,26 +27,33 @@ class CustomTableView: UIView, UITableViewDelegate, UITableViewDataSource {
     
     private func commonInit() {
         
-//        Bundle.main.loadNibNamed(, owner: self, options: nil)
-//        addSubview(contentView)
-//        contentView.frame = self.bounds
-//        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-//        self.layer.backgroundColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0)
-//
-//        collectionView.delegate = self
-//        collectionView.dataSource = self
-//
-//        let collectionViewCellNib = UINib(nibName: "CustomCollectionViewCell", bundle: nil)
-//        self.collectionView.register(collectionViewCellNib, forCellWithReuseIdentifier: "customCollectionViewCell")
-//
-//        debugPrintString("commonInitCalled")
+        Bundle.main.loadNibNamed(CustomTableView.tableViewID, owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        self.layer.backgroundColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0)
+        
+        tableViewCustom.delegate = self
+        tableViewCustom.dataSource = self
+        
+        cellInit()
     }
-
+    
+    private func cellInit() {
+        let tableViewCellNib = UINib(nibName: CustomTableViewCell.cellTblID, bundle: nil)
+        self.tableViewCustom.register(tableViewCellNib, forCellReuseIdentifier: CustomTableViewCell.cellTblID)
+    }
+    
+//    MARK: - Tableview datasource/delegate
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         <#code#>
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         <#code#>
     }
 }
